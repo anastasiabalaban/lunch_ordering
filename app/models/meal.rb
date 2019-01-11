f4b17a7c611836af63b3dedae8e34f8a28f1a55e
anastasiabalaban <anastasiabalaban07@gmail.com>
2019-01-11 14:41:05 +0200

    fix meals

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -7,9 +7,9 @@ class Meal < ApplicationRecord
   has_many :meals_orders
   has_many :orders, through: :meals_orders
 
-  scope :first_meals, -> { joins(:item).where(items: { meal_type: Item::FIRST_MEAL }) }
-  scope :main_meals,  -> { joins(:item).where(items: { meal_type: Item::MAIN_MEAL }) }
-  scope :drinks,      -> { joins(:item).where(items: { meal_type: Item::DRINK }) }
+  scope :first_meals, -> { Orders::FirstMeals.call }
+  scope :main_meals,  -> { Orders::MainMeals.call }
+  scope :drinks,      -> { Orders::Drinks.call }
 
   accepts_nested_attributes_for :item, reject_if: :all_blank
 
